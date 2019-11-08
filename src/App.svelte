1f120bf11267e2d5cd3f856dbf103c1e9408facf
Matt Forrester <github.com@speechmarks.com>
2019-11-08 16:12:54 +0000

    style: fix indenting in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -175,142 +175,142 @@
 
   <div class="off-canvas-content">
 
-  {#if ($viewStore.result || {}).status == "error"}
-  <div class="columns">
-    <div class="column col-auto" style="margin: 3rem auto 0 auto">
-      <div class="toast toast-error">
-        <button class="btn btn-clear float-right"></button>
-        <div style="padding: 1em">
-          { $viewStore.result.message }
+    {#if ($viewStore.result || {}).status == "error"}
+    <div class="columns">
+      <div class="column col-auto" style="margin: 3rem auto 0 auto">
+        <div class="toast toast-error">
+          <button class="btn btn-clear float-right"></button>
+          <div style="padding: 1em">
+            { $viewStore.result.message }
+          </div>
         </div>
       </div>
     </div>
-  </div>
-{/if}
+    {/if}
 
 
-<div class={ ($viewStore.asPopup) ? "modal active in-popup" : "no-modal" }>
-<a href="#close" on:click|preventDefault={cancel} class="modal-overlay" aria-label="Close">&nbsp;</a>
-<div class="columns" style={ $viewStore.asPopup ? "" : "margin-top: 2rem"}>
-  <div class="column col-auto modal-container code-popup" style="margin: auto">
+    <div class={ ($viewStore.asPopup) ? "modal active in-popup" : "no-modal" }>
+      <a href="#close" on:click|preventDefault={cancel} class="modal-overlay" aria-label="Close">&nbsp;</a>
+      <div class="columns" style={ $viewStore.asPopup ? "" : "margin-top: 2rem"}>
+        <div class="column col-auto modal-container code-popup" style="margin: auto">
 
-    <div class="modal-header">
-      <div class="container"><div class="columns">
-          <div class="column col-9">
-            <h3>{ $viewStore.definition.title }</h3><div id="color-finder" style="display: none"><span id="color-finder-error" class="text-error"></span>a<span id="color-finder-warning" class="text-warning">b</span><span id="color-finder-success" class="text-success">c</span></div>
+          <div class="modal-header">
+            <div class="container"><div class="columns">
+                <div class="column col-9">
+                  <h3>{ $viewStore.definition.title }</h3><div id="color-finder" style="display: none"><span id="color-finder-error" class="text-error"></span>a<span id="color-finder-warning" class="text-warning">b</span><span id="color-finder-success" class="text-success">c</span></div>
+                </div>
+                <div class="column col-3">
+                  <label class="form-switch" style="float: right">
+                    <input type="checkbox" bind:checked={showingSql}>
+                    SQL<i class="form-icon"></i>
+                  </label>
+                </div>
+              </div></div>
           </div>
-          <div class="column col-3">
-            <label class="form-switch" style="float: right">
-              <input type="checkbox" bind:checked={showingSql}>
-              SQL<i class="form-icon"></i>
-            </label>
-          </div>
-        </div></div>
-    </div>
 
-    <div class="modal-body">
-      {#if showingSql}
-      <div class="container"><div class="col-gapless columns">
-          <div class="code-code column col-12" id="code-input-area">
-            {#each $viewStore.statement as line}
-            <div class="line">
-              {#each line as item}
-              {#if typeof item == "string"}
-              <Highlighter parameters={$viewStore.definition.parameters} item={item}/>
-              {:else}
-              <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[item.name]} parameter={item}/>
-              {/if}
+          <div class="modal-body">
+            {#if showingSql}
+            <div class="container"><div class="col-gapless columns">
+                <div class="code-code column col-12" id="code-input-area">
+                  {#each $viewStore.statement as line}
+                  <div class="line">
+                    {#each line as item}
+                    {#if typeof item == "string"}
+                    <Highlighter parameters={$viewStore.definition.parameters} item={item}/>
+                    {:else}
+                    <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[item.name]} parameter={item}/>
+                    {/if}
+                    {/each}
+                  </div>
+                  {/each}
+                </div>
+              </div>
+            </div>
+            {:else}
+            <div class="col-gapless columns code-description">
+              <div class="column-12" id="code-input-area">
+                <div>{@html md.render("" + $viewStore.definition.description) }</div>
+              </div>
+            </div>
+            <div class="form-horizontal code-form">
+              {#each $viewStore.definition.parameters as parameter}
+              <div class="form-group">
+                <div class="column col-5 col-sm-12">
+                  <label class="form-label" for={ "input-" + parameter.name }>{parameter.name}</label>
+                </div>
+                <div class="column col-7 col-sm-12">
+                  {#if parameter.type == "static"}
+                  <label class="form-label" >
+                  <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[parameter.name]} parameter={parameter}/>
+                  </label>
+                  {:else}
+                  <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[parameter.name]} parameter={parameter}/>
+                  {/if}
+                </div>
+              </div>
               {/each}
             </div>
-            {/each}
+            {/if}
+          </div>
+            {#if $viewStore.asPopup}
+            <ResultTable controls={$viewStore.controls} pick={pick} inPopup={true} definition={$viewStore.definition} result={$viewStore.result}/>
+            {/if}
+
+
+
+          <div class="modal-footer">
+            <div class="container"><div class="col-gapless columns"><div class="column col-12">
+            {#if $viewStore.asPopup}
+            <button class="btn btn-link" on:click={cancel}>Cancel</button>
+            {/if}
+            <button class={ "btn btn-primary " + buttonClass($viewStore.definition.statement_type) } on:click={run}>
+              { $viewStore.definition.statement_type || "EXECUTE" }
+            </button>
+            </div></div></div>
           </div>
         </div>
       </div>
-      {:else}
-      <div class="col-gapless columns code-description">
-        <div class="column-12" id="code-input-area">
-          <div>{@html md.render("" + $viewStore.definition.description) }</div>
-        </div>
-      </div>
-      <div class="form-horizontal code-form">
-        {#each $viewStore.definition.parameters as parameter}
-        <div class="form-group">
-          <div class="column col-5 col-sm-12">
-            <label class="form-label" for={ "input-" + parameter.name }>{parameter.name}</label>
-          </div>
-          <div class="column col-7 col-sm-12">
-            {#if parameter.type == "static"}
-            <label class="form-label" >
-            <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[parameter.name]} parameter={parameter}/>
-            </label>
+    </div>
+
+    {#if $viewStore.showingDownload}
+    <div class="modal active" id="modal-id">
+      <a href="#close" class="modal-overlay" aria-label="Close" on:click|preventDefault={cancelDownload}>&nbsp;</a>
+      <div class="modal-container">
+        <div class="modal-header">
+          <a href="#close" on:click|preventDefault={cancelDownload} class="btn btn-clear float-right" aria-label="Close">&nbsp;</a>
+          <div class="modal-title h5">
+            {#if getDownloads($viewStore.result).length }
+              Please select a download
             {:else}
-            <Parameter on:newvalue={onnewcontrolvalue} onfix={onfix} onerror={onerror} onfocus={onfocus} onblur={onblur} popup={popup} bind:control={$viewStore.controls[parameter.name]} parameter={parameter}/>
+              Preparing downloads
             {/if}
           </div>
         </div>
-        {/each}
-      </div>
-      {/if}
-    </div>
-      {#if $viewStore.asPopup}
-      <ResultTable controls={$viewStore.controls} pick={pick} inPopup={true} definition={$viewStore.definition} result={$viewStore.result}/>
-      {/if}
-
-
-
-    <div class="modal-footer">
-      <div class="container"><div class="col-gapless columns"><div class="column col-12">
-      {#if $viewStore.asPopup}
-      <button class="btn btn-link" on:click={cancel}>Cancel</button>
-      {/if}
-      <button class={ "btn btn-primary " + buttonClass($viewStore.definition.statement_type) } on:click={run}>
-        { $viewStore.definition.statement_type || "EXECUTE" }
-      </button>
-      </div></div></div>
-    </div>
-  </div>
-</div>
-</div>
-
-{#if $viewStore.showingDownload}
-<div class="modal active" id="modal-id">
-  <a href="#close" class="modal-overlay" aria-label="Close" on:click|preventDefault={cancelDownload}>&nbsp;</a>
-  <div class="modal-container">
-    <div class="modal-header">
-      <a href="#close" on:click|preventDefault={cancelDownload} class="btn btn-clear float-right" aria-label="Close">&nbsp;</a>
-      <div class="modal-title h5">
-        {#if getDownloads($viewStore.result).length }
-          Please select a download
-        {:else}
-          Preparing downloads
-        {/if}
-      </div>
-    </div>
-    <div class="modal-body">
-      <div class="content" style="min-height: 2rem">
-        {#if getDownloads($viewStore.result).length }
-          {#each getDownloads($viewStore.result) as link}
-            <a href="#download" on:click|preventDefault={runDownload(link.type)}>{link.type}</a>
-          {/each}
-        {:else}
-          <div style="text-align: center; font-size: 144pt;">
-            🕓
+        <div class="modal-body">
+          <div class="content" style="min-height: 2rem">
+            {#if getDownloads($viewStore.result).length }
+              {#each getDownloads($viewStore.result) as link}
+                <a href="#download" on:click|preventDefault={runDownload(link.type)}>{link.type}</a>
+              {/each}
+            {:else}
+              <div style="text-align: center; font-size: 144pt;">
+                🕓
+              </div>
+            {/if}
           </div>
-        {/if}
+        </div>
+        <div class="modal-footer">
+          <a href="#close" on:click|preventDefault={cancelDownload} aria-label="Close">Cancel</a>
+        </div>
       </div>
     </div>
-    <div class="modal-footer">
-      <a href="#close" on:click|preventDefault={cancelDownload} aria-label="Close">Cancel</a>
-    </div>
-  </div>
-</div>
-{/if}
+    {/if}
 
-{#if !$viewStore.asPopup}
-<div style="margin-top: 3em" class={ ($viewStore.asPopup) ? "in-popup" : "" }>
-  <ResultTable showDownloads={showDownloads} controls={$viewStore.controls} inPopup={false} definition={$viewStore.definition} result={$viewStore.result}/>
-</div>
-{/if}
+    {#if !$viewStore.asPopup}
+    <div style="margin-top: 3em" class={ ($viewStore.asPopup) ? "in-popup" : "" }>
+      <ResultTable showDownloads={showDownloads} controls={$viewStore.controls} inPopup={false} definition={$viewStore.definition} result={$viewStore.result}/>
+    </div>
+  {/if}
   </div>
 </div>
 
